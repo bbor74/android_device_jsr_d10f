33552f0608629d957fc00264c02472172e640c9e
LuK1337 <priv.luk@gmail.com>
2021-12-15 18:20:23 +0100

    d10f: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.

--- device.mk
+++ device.mk
@@ -193,7 +193,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/vendor/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/vendor/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/vendor/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/vendor/etc/permissions/android.hardware.bluetooth_le.xml \
