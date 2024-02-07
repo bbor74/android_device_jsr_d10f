3390e4fa565a6c8aab619205360e3cd87b4b1d21
Boris Belyaev <bbor1974@gmail.com>
2024-02-07 14:55:13 +0400

    [OVERLAY] Settings: Show JSR_Settings page in "About phone"
    Overriding additional_system_update_settings is much easier than adding new entry in main menu
    
    Change-Id: I4399b0ebee0bebc47192ff5cb8728ac5aac936d3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -55,8 +55,8 @@ done
 popd > /dev/null
 echo "Applying patches: done"
 
-#echo "Updating overlay"
-#sh device/jsr/d10f/update-overlay.sh
+echo "Updating overlay"
+sh device/jsr/d10f/update-overlay.sh
 
 rm -f out/target/product/d10f/system/build.prop out/target/product/d10f/boot/root/default.prop out/target/product/d10f/recovery/root/default.prop
 
