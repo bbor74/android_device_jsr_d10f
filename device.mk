2934abbe77346b1c883ff6041283848172c9f03d
Boris Belyaev <bbor1974@gmail.com>
2024-03-20 17:53:33 +0400

    update for USB

--- device.mk
+++ device.mk
@@ -270,6 +270,18 @@ PRODUCT_PACKAGES += \
     libshims_thermal \
     libshims_atomic
 
+# USB ID
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.usb.vid=2717 \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=ff60 \
+    ro.usb.id.mtp_adb=ff68 \
+    ro.usb.id.ptp=ff10 \
+    ro.usb.id.ptp_adb=ff18 \
+    ro.usb.id.ums=ff20 \
+    ro.usb.id.ums_adb=ff28
+
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
