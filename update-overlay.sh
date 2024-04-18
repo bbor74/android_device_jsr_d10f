67cd3f7c13a10a25021f8290f68f33024fa56d1c
Boris Belyaev <bbor1974@gmail.com>
2024-04-18 13:21:01 +0400

    update-overlay

--- update-overlay.sh
+++ update-overlay.sh
@@ -9,8 +9,4 @@ mkdir -p overlay/packages/apps/Settings/res/xml/
 
 xmlstarlet ed --pf --ps \
 -u '/PreferenceScreen/Preference[@android:key="additional_system_update_settings"]/@android:title' -v "@string/wifi_show_advanced" \
-../../../packages/apps/Settings/res/xml/device_info_settings.xml > overlay/packages/apps/Settings/res/xml/device_info_settings.xml
-
-xmlstarlet ed --pf --ps \
--u '/PreferenceScreen/Preference[@android:key="additional_system_update_settings"]/@android:title' -v "@string/wifi_show_advanced" \
 ../../../packages/apps/Settings/res/xml/system_dashboard_fragment.xml > overlay/packages/apps/Settings/res/xml/system_dashboard_fragment.xml
