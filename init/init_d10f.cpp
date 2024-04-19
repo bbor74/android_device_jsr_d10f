501d828ac18ea91764076331e850d1ed6203244f
Boris Belyaev <bbor1974@gmail.com>
2024-04-19 15:49:42 +0400

    init: Fix compile error
    device/jsr/d10f/init/init_d10f.cpp:63:21: error: use of undeclared identifier 'ANDROID_TARGET'

--- init/init_d10f.cpp
+++ init/init_d10f.cpp
@@ -60,7 +60,7 @@ void vendor_load_properties()
     // Init a dummy BT MAC address, will be overwritten later
     property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
     std::string platform = GetProperty("ro.board.platform", "");
-    if (platform != ANDROID_TARGET)
+    if (platform != "msm8226")
         return;
 
     property_override_dual("ro.product.model", "ro.vendor.product.model", "D10F");
