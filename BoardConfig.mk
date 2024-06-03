4b2b931b5fc33695619b84b1b3eadb0b3023d8e1
Boris Belyaev <bbor1974@gmail.com>
2024-06-03 13:26:54 +0400

    Enable workaround for old MCC/MNC format
     * Fixes manual network selection

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,6 +190,7 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # Radio
 #TARGET_RIL_VARIANT := caf
+TARGET_USES_OLD_MNC_FORMAT := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.full
