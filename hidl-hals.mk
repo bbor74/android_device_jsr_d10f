3def2e125c34734c9443330b46077d1bdeff7d0a
Boris Belyaev <bbor1974@gmail.com>
2024-12-06 08:51:16 +0400

    d10f: Switch to Audio HAL V6

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -16,9 +16,9 @@
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.broadcastradio@1.0-impl
 
 # Bluetooth
